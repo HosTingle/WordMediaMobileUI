ba403de190e287a5a8d92242cddaf8ef9e27bed9
Gökdeniz Genç <fbgokdeniz@gmail.com>
2024-06-01 18:56:51 +0300

    inital

--- lib/pages/view/login/splash_screen.dart
+++ lib/pages/view/login/splash_screen.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import '../../../base/constant.dart';
 import '../../routes/app_routes.dart';
 class SplashScreen extends StatefulWidget {
   @override
